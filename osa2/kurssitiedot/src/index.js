0e0cf0bb42bc1813194d1e8db1dcc99f8eed23cf
TommiON <44175014+TommiON@users.noreply.github.com>
2019-04-09 16:02:41 +0300

    Full Stack 2019, osa 2

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Course from './components/Course'
+
+const App = () => {
+    const courses = [
+        {
+          name: 'Half Stack -sovelluskehitys',
+          id: 1,
+          parts: [
+            {
+              name: 'Reactin perusteet',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Tiedonvälitys propseilla',
+              exercises: 7,
+              id: 2
+            },
+            {
+              name: 'Komponenttien tila',
+              exercises: 14,
+              id: 3
+            }
+          ]
+        },
+        {
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: 'Middlewaret',
+              exercises: 7,
+              id: 2
+            }
+          ]
+        },
+        {
+            name: 'Testikurssi',
+            id: 3,
+            parts: [
+              {
+                name: 'xxx',
+                exercises: 3,
+                id: 1
+              },
+              {
+                name: 'yyy',
+                exercises: 7,
+                id: 2
+              }
+            ]
+          }
+      ]
+
+  return (
+      courses.map(kurssi => {
+        return(
+            <div key={kurssi.id}>
+                <Course course={kurssi} />
+            </div>
+        )
+      })
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
