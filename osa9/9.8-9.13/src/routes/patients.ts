75dd0ad03f4b8aad5a689d845f3f4890893945f1
Tommi Niittymies <tommi.niittymies@gmail.com>
2021-03-14 20:10:36 +0200

    Exercise 9.12

--- osa9/9.8-9.13/src/routes/patients.ts
+++ osa9/9.8-9.13/src/routes/patients.ts
@@ -6,6 +6,19 @@ const router = express.Router();
 router.get('/', (_req, res) => {
     const response = patientService.getEntries();
     res.send(response);
-})
+});
+
+router.post('/', (req, res) => {
+    const {name, dateOfBirth, ssn, gender, occupation} = req.body;
+    const response = patientService.addEntry(
+        name,
+        dateOfBirth,
+        ssn,
+        gender,
+        occupation
+    );
+    res.send(response);
+   
+});
 
 export default router;
--- osa9/9.8-9.13/src/services/patientService.ts
+++ osa9/9.8-9.13/src/services/patientService.ts
@@ -1,5 +1,7 @@
 import patientData from '../../data/patients.json';
 import {Patient} from '../types/types';
+import {v1 as uuid} from 'uuid';
+const id = uuid();
 
 const getEntries = ():Omit<Patient, 'ssn'>[] => {
     return patientData.map(({id, name, occupation, gender, dateOfBirth}) => ({
@@ -12,4 +14,18 @@ const getEntries = ():Omit<Patient, 'ssn'>[] => {
     );
 }
 
-export default {getEntries};
+const addEntry = (name: string, dateOfBirth: string, ssn: string, gender: string, occupation: string) => {
+    const patient = {
+        id: id,
+        name: name,
+        dateOfBirth: dateOfBirth,
+        ssn: ssn,
+        gender: gender,
+        occupation: occupation
+    }
+    patientData.push(patient);
+    console.log('uusi potilas: ', patient);
+    return patient;
+}
+
+export default {getEntries, addEntry};
